9238c92dd3e5e1a42ad54e9efb65e4e110e88b0c
ugwuintech <josephchukwuebukaugw@gmail.com>
2023-01-27 17:36:38 +0000

    added the changeTheme function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var tasks = [];
 tasks = JSON.parse(localStorage.getItem("allTasks"));
 tasks = tasks == null ? [] : tasks;
+const select = (ele) => document.querySelector(ele);
 const render = (data) => {
   const element = document.getElementById("content");
   element.innerHTML = "";
@@ -70,3 +71,42 @@ const trash = (id) => {
     render(tasks);
   }
 };
+
+// settings tray
+
+const tray = document.getElementById("settings-tray");
+const hero = document.getElementById("hero");
+const addBtn = document.getElementById("add-todo");
+const newTask = document.getElementById("new-task-btn");
+const saveBtn = document.getElementById("save-btn");
+
+function openTray() {
+  tray.style.width = "225px";
+  lightTheme.style.display = "none";
+}
+function closeTray() {
+  tray.style.width = "50px";
+  lightTheme.style.display = "block";
+}
+
+function redTheme() {
+  hero.className = "hero red-bg";
+  addBtn.className = "btn task-btn red-bg";
+  newTask.className = "btn task-btn float-end red-bg";
+  saveBtn.className = "btn task-btn red-bg";
+  closeTray();
+}
+function greenTheme() {
+  hero.className = "hero green-bg";
+  addBtn.className = "btn task-btn green-bg";
+  newTask.className = "btn task-btn float-end green-bg";
+  saveBtn.className = "btn task-btn green-bg";
+  closeTray();
+}
+function yellowTheme() {
+  hero.className = "hero yellow-bg";
+  addBtn.className = "btn task-btn yellow-bg";
+  newTask.className = "btn task-btn float-end yellow-bg";
+  saveBtn.className = "btn task-btn yellow-bg";
+  closeTray();
+}
